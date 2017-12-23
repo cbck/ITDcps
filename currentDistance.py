c0a535eddfacba2b75ff550539fba46b421ec6eb
Melissa <33120665+MelSen584@users.noreply.github.com>
2017-12-23 06:43:16 +0100

    Add files via upload

---
+++ currentDistance.py
@@ -0,0 +1,27 @@
+'''
+Distanzbestimmung
+currentDistance.py
+@author: Melissa & Rojda
+'''
+
+                    #import math
+
+import time
+t = time.time()     
+
+                    #import velocity -Geschwindigkeit 
+v = 42              #getVelocity()   -42 nur Beispiel 
+
+
+def startdistance(): sd (500)
+sd = startdistance.startdistance() #distance from startpoint to trafficlight (in centimeters) 
+
+def newdistance(): nd   
+nd = newdistance.newdistance()  
+                        
+
+#while nd >= 0:    -über Aufruf einigen, Schleife oder manueller Aufruf
+nd = sd - (v*t) 
+
+print ("Derzeitige Distanz (in cm):",nd)
+
