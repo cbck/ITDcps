96e22c9900531dc246cc7e2cf565f921df21cd92
ChrisBajo <c-b-e@gmx.net>
2018-01-12 11:18:19 +0100

    new master from cbe_latenight

--- code/main.py
+++ code/main.py
@@ -10,83 +10,98 @@
 #from MbotMQTT import mbot_drive_straight
 #from MbotMQTT import mbot_motor_stop
 #from carSubscriber import connectMQTT
+#import carSubscriberLibrary
+from carSubscriberLibrary import connectMQTT
+from carSubscriberLibrary import on_message
+from carSubscriberLibrary import driveWithQueue
+from carSubscriberLibrary import testQueue
+from carSubscriberLibrary import getTTNS
 import Queue
 import array
 import pickle
-from thread import start_new_thread
-from carSubscriberLibrary import readIO
-from carSubscriberLibrary import connectMQTT
+#from thread import start_new_thread
+import threading
+#from carSubscriberLibrary import readIO
+#from carSubscriberLibrary import connectMQTT
 from time import sleep
+Broker="172.31.12.122"
 workArray = []
-bufFloat=[]
 f = "io_file.txt"
 #distance = 50.0
 #workArray contents [Red,RedYellow,Yellow,Green,ttns,currentState]
-
+q = Queue(maxsize =0)
 
 def main():
+
+	print "Main Started here"
+
+	q = queue.LifoQueue()
+	q.put(getTTNS)
+
+	print "*** LIFO Queue created"
+
 	#os.system('python carSubscriber.py')
 	print "carSubscriber.py should start"
+	mqttConnection = connectMQTT(Broker)
+	mqttConnection.start()
 	
+	threading.Thread(target=process_ttns, args=(q,))
 	
-	print "Main Started here"
-	q = Queue.Queue()
-	#start carSubscriber.py as shell process here!
-	#file must be in same working director
+	#q = Queue.Queue()
+	driveWithQueue()
+	testQueue()
+def process_ttns(q):
+	while True:
+		next_ttns = q.get()
+		print('*** TTNS from q is:', next_ttns.getTTNS)
+		q.task_done()
+
+	'''
 	try:
 		print "Testausgabe 0"
 		#Im Moment carSubsriber selber aufrufen
-		#start_new_thread(connectMQTT,())
-
+		#start_new_thread(connectMQTT,(self,Broker))
+			
 		print "Testausgabe 1"
 		#thread.start_new_thread(driveAlgorithm, ())
 
 		print "Testausgabe 2"
-		start_new_thread(printIO,())
+		#start_new_thread(printIO,())
 		
 	except:
 		print "Error: unable to start thread"
 
 	while 1:
 		pass
+	'''
 
-def timeNextGreenDeadline():
-	#Function returns a float with time in seconds till the next Green-Phase ends
-	if 	workArray[5] == "Green":
-		return 0 
-		print("--------------------------------------------------Green received in main.py")
 
-	if workArray[5] == "Red":
-		#return ((timeRed - ttns) + timeRedYellow)
-		return (workArray[0] - workArray[4] +workArray[1])
-		print("--------------------------------------------------Red received in main.py")
-
-	if workArray[5] == "Yellow":
-		#return ((timeYellow -ttns) + timeRed + timeRedYellow)
-		return (workArray[2] - workArray[4] + workArray[0] + workArray[1])
-		print("--------------------------------------------------Yellow received in main.py")
-
-		
-	if workArray[5] == "Red-Yellow":
-		#return ttns
-		return workArray[4]
-		print("--------------------------------------------------Red-Yellow received in main.py")
-	else:
-		return "Time till next Green Deadline can not be calculated"
-
+'''
 def driveAlgorithm():
 	#Written by Johannes
 	#Distance wird fix angenommen
-	#Geschwindigkeitsalgorithmus mit korrekten Werten fehlt noch
 	print("Drive Algorithm started here")
-	#mbot_drive_straight(my_mbot,serial,255,"forward");
 	
+    if timeNextGreenState == "error":
+		break
+	
+	elif CS == "Green":
+		#workArray[4] ist TTNS
+	  if workArray[4] > distance/mbot_speed: 
+		 mbot_drive_straight(my_mbot,serial,mbot_speed,"foreward")
+		  	
+	elif timeNextGreenState > distance/mbot_speed:
+		if 
+		mbot_speed = distance/timeNextGreenDeadline
+		mbot_drive_straight(my_mbot,serial,mbot_speed,"foreward")		
+'''
+'''	
 def printIO():
 	while 1:
 		io_data = readIO(f)
 		print io_data
 		sleep(0.01)
-	
+'''
 
 
 
