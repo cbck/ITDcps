ff8d7dae03b46a0f088ca3b1ef36ae4f7dae2477
ChrisBajo <christopher.beck@stud.hshl.de>
2018-01-15 18:25:47 +0100

    Wunderbärchen: Die Thread einlesung von MQTT klappt soweit und die Variablen stehen zur verfügung. 	       Die main-johannes triggert durch while1: in Zeile 132 nur noch den mBot und braucht dort dann auch eine Threadimplementierung, damit beides gleichzeitig laufen kann.
    	       mboty.py bitte immer verwenden und keine altlasten aufleben lassen ;)
    	       Wie verhält sich das jetzt mit den Akkus? Immer ausmachen wenn man etwas getestet hat am mBot und wenn möglich laden :)
    		Zitat: J.Schaefer: "Des woars [für Heute]. - [1] anmerkung des Autors

--- main_johannes.py
+++ main_johannes.py
@@ -1,33 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  main.py
-#  
-#  Copyright 2017  <pi@raspberrypi>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
 
+import paho.mqtt.client as mqtt
 #import MbotMQTT
-from MbotMQTT import mbot_drive_straight
-from MbotMQTT import mbot_motor_stop
-from MbotMQTT import Mbot_start
+from mbot import mbot_drive_straight
+from mbot import mbot_motor_stop
+#from MbotMQTT import Mbot_start
 #from carSubscriberLibrary import readIO
 import time
+import serial
+import threading
+
+serial = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+
 workArray=[ 10.,1.,3.,10.,5.,"Green"]
 
 start_distance = 50.0
@@ -36,30 +19,130 @@ start_speedsi = (0.42/255)*155
 max_speed = 255
 min_speed = 55
 min_speedsi = (0.42/255)*55
+noInterupt = True
+Broker="172.31.12.122"
+topic_pub="traffiLight/carMessage"
+#subscribe topics
+state = "traffiLight/state"             #dummy topic
+TTNS = "traffiLight/timeTillNextState"  #refreshed subtopic showing time till change in seconds
+timestamp = "traffiLight/timestamp"               #UTC Servertime from Trafficlight
+iRed = "traffiLight/iRed"               #interval for Red in Seconds
+iRedYellow = "traffiLight/iRedYellow"   #interval for Red-Yellow in Seconds
+iGreen = "traffiLight/iGreen"           #interval for Green in Seconds
+iYellow = "traffiLight/iYellow"         #interval for Yellow in Seconds
+interupt = "traffiLight/interupt"       #interupt if Bus/RTW comes and other
+CS = "traffiLight/currentState"
+currentState = ""
+timeRed = 0.0
+timeRedYellow = 0.0
+timeGreen = 0.0
+timeYellow = 0.0
+	
+def on_connect(client, userdata, flags, rc):
+		print("Connected with result code "+str(rc))
+		#self.client.subscribe(timestamp)
+		client.subscribe(iRed)
+		client.subscribe(iRedYellow)
+		client.subscribe(iGreen)
+		client.subscribe(iYellow)
+		client.subscribe(TTNS)
+		client.subscribe(interupt)
+		client.subscribe(CS)
+		
+def on_message(client, userdata, msg):
+	'''
+	if self.msg.topic == timestamp:
+			servertime = str(msg.payload)
+			print("UTC Servertime:" +servertime)
+	'''
+
+	if msg.topic == iRed: 
+		timeRed = msg.payload
+		#print("Red: " + timeRed)
+		workArray[0] = timeRed
+	
+	if msg.topic == iRedYellow:
+		timeRedYellow = msg.payload
+		#print("RedYellow: " + timeRedYellow)
+		workArray[1] = timeRedYellow
+
+	if msg.topic == iYellow:
+		timeYellow = msg.payload
+		#print("Yellow: " + timeYellow)
+		workArray[2] = timeYellow
+
+	if msg.topic == iGreen:
+		timeGreen = msg.payload
+		#print("Green: " + timeGreen)
+		workArray[3] = timeGreen
+
+	if msg.topic == interupt:
+		serverInterupt = msg.payload
+		#print("Serverinterupt " + serverInterupt)
+
+	if msg.topic == TTNS:
+		ttns = msg.payload
+		#print("timeTillNextState " + ttns +"[seconds]")
+		workArray[4] = ttns
+
+	if msg.topic == CS:
+		currentState = msg.payload
+		#print("currentState " + currentState)
+		workArray[5] = currentState
+		
+
+
+class connectMQTT(threading.Thread):
+	def __init__(self,Broker):
+		threading.Thread.__init__(self)
+		
+		if Broker == 0:
+			self.Broker="172.31.12.122"
+		
+		self.client = mqtt.Client()
+		self.client.on_connect = on_connect
+		self.client.on_message = on_message
+		self.client.connect(Broker, 1883, 60)
+		self.run()
+		
+	def run(self):
+		#while noInterupt == True:
+		#print(timeNextGreenDeadline())
+		self.client.loop_start()
+		time.sleep(0.1)
+		print str(workArray)
+		self.client.loop_stop()
+
 
 current_milli_time = lambda: int(round(time.time() * 1000))
 
 
 def main():
-	driveAlgorithm()
+	print "Chris Threadings should start here"
+	mqttConnection = connectMQTT(Broker)
+	mqttConnection.start()
+	print "Chris Thread started"
+	
+	#driveAlgorithm()
 
 	
 def driveAlgorithm():
 	t0 = current_milli_time()
    #while Mbot_start() == 1:
 	while 1:
-		if timeNextGreenStart == "error":
+		print "Mbot started!"
+		if timeNextGreenStart() == "error":
 			break
 		else:
-			mbot_drive_straight(my_mbot,serial,start_speed,"foreward")		    
+			mbot_drive_straight(serial,start_speed,"forward")		    
 			tta = start_distance/start_speedsi
-		
-			# wenn die Grünphase erreicht werden kann, dann behalte Geschwindigkeit
+			print "Debug1"
+			# wenn die Gruenphase erreicht werden kann, dann behalte Geschwindigkeit
 			if timeNextGreenStart()< tta and workArray[4] > tta:
-				mbot_drive_straight(my_mbot,serial,start_speed,"foreward")
-		
-			#Mbot würde vor oder nach der Grünphase ankommen
-			elif timeNextGreenStart() > tta or workarray[4] < tta:
+				mbot_drive_straight(serial,start_speed,"forward")
+				print "Debug2"
+			#Mbot wuerde vor oder nach der Gruenphase ankommen
+			elif timeNextGreenStart() > tta or workArray[4] < tta:
 			
 			#Mbot muss an der Ampel stoppen
 				if timeNextGreenStart() > start_distance/min_speed:
@@ -69,10 +152,11 @@ def driveAlgorithm():
 					if distance >= start_distance:
 						mbot_motor_stop()
 					
-			#Geschwindigkeit wird angepasst, sodass Mbot bei grün ankommt		
+			#Geschwindigkeit wird angepasst, sodass Mbot bei gruen ankommt		
 			else:
 				new_speed = distance/timeNextGreenStart()
-				mbot_drive_straight(my_mbot,serial,new_speed,"foreward")
+				mbot_drive_straight(serial,new_speed,"forward")
+				print "Debug3"
 		
 
 def timeNextGreenStart():
@@ -98,15 +182,14 @@ def timeNextGreenStart():
 		print("--------------------------------------------------Red-Yellow received in main.py")
 	else:
 		return "Time till next Green Deadline can not be calculated"
+		
 def slowdown():
 	for new_speed in range(start_speed,min_speed):
-		mbot_drive_straight(my_mbot,serial,new_speed,"foreward")
+		mbot_drive_straight(serial,new_speed,"foreward")
 		new_speed= newspeed-1
 		sleep(0.2)
-	 
-	
-
 
 if __name__ == '__main__':
 	main()
+
 
