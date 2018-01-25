40a167097628576cdef557ea9715c930aa38b585
daniel-muenstermann <33104367+daniel-muenstermann@users.noreply.github.com>
2018-01-25 19:48:42 +0100

    Add files via upload
    sollte funktionieren ;)

--- threading_test.py
+++ threading_test.py
@@ -145,7 +145,7 @@ class Get_MQTT:                         # Begining of the Get_MQTT Class
         while self._running:            # Start While Loop
 
             self.client.loop_start()
-            print str(workArray)		# Prints out the recived Messages            
+            #print str(workArray)		# Prints out the recived Messages            
             self.client.loop_stop()
             
             """
@@ -164,8 +164,14 @@ class Get_MQTT:                         # Begining of the Get_MQTT Class
 
 class Drive_Algorithm:
 
+	def __init__(self):
+		self._running = True
+
+	def terminate(self):                # Terminator for the Drive_Algortihm_Thread
+		self._running = False
+
 	#This functions tooks an Integer Value between 0 and 255 and returns a Speed Value in M/S
-	def int2speed(int_speed, max_power_speed):
+	def int2speed(self, int_speed, max_power_speed):
 		#Calculate the M/S rate for each step between 0 and 255
 		 units_per_val = max_power_speed/255
 		 #Calculate the actual Speed 
@@ -173,20 +179,44 @@ class Drive_Algorithm:
 		 return speed
 
 	#This function tooks an Speed Value in M/s and generates a Integer Value between 0 and 255
-	def speed2int(speed, max_power_speed):
+	def speed2int(self, speed, max_power_speed):
 		#Calculate the M/S rate for each Step between 0 and 255 
 		units_per_val = max_power_speed/255
 		#calculate the Integer Value for the Mbot
 		int_speed = speed / units_per_val
 		return int(int_speed)
+	
+	#Function returns a float with time in seconds till the next Green-Phase ends	
+	def timeNextGreenStart(self):
+		
+		print str(workArray)
+		
+		if 	workArray[5] == "Green":
+			result = float(workArray[0])+ float(workArray[4]) + float(workArray[1]) + float(workArray[2])
+			return result
+		elif workArray[5] == "Red":
+			#return (ttns + timeRedYellow)
+			result = float(workArray[4]) + float(workArray[1])
+			return result
+		elif workArray[5] == "Yellow":
+
+			result = float(workArray[4]) + float(workArray[0]) + float(workArray[1])
+			return result
+		elif workArray[5] == "Red-Yellow":
+
+			result = float(workArray[4])
+			return result
+		else:
+			return 0.0
 
 	#This function checks if it is possible to reach the next Green light with max Speed and 
 	#gives back the earliest time the mbot could reach the trafficlight
-	def calc_reachable_green_phase(speed, distance, time_to_next_green_start, time_green_interval):
+	def calc_reachable_green_phase(self, speed, distance, time_to_next_green_start, time_green_interval):
 		#first of all calculate the time to reach with the setted Speed to arrive at the trafficlight
 
 		#init a Var for saving our new time
-		time_to_earliest_green_start = 0
+		time_to_earliest_green_start = 0.0
+		time_to_next_green_start = float(time_to_next_green_start)
 
 		#while forever
 		while (1):
@@ -196,12 +226,17 @@ class Drive_Algorithm:
 				break
 
 			#otherwise add the intervaltime for the next greenphase to the Variable
-			time_to_earliest_green_start = time_to_earliest_green_start + time_green_interval
+			time_to_earliest_green_start = float(time_to_next_green_start) + time_green_interval
+			print "countdown"
+			print 
+			
+			print "Jetzt kommt die Zeit"
+			print time_to_earliest_green_start
 			
 		return time_to_earliest_green_start
 		
 	#This Function tooks the initial start speed, the time to decide,  and the distance
-	def calc_rest_distance(start_speed, distance,t_decide):
+	def calc_rest_distance(self, start_speed, distance,t_decide):
 
 		#at first calc the distance the Mbot has driven after he reaches t_decide 
 		distance_before_decision = start_speed * t_decide
@@ -211,7 +246,7 @@ class Drive_Algorithm:
 		return rest_distance
 
 	#This Funktion is the real Drive Agorithm
-	def calc_new_speed(rest_distance, rest_time, time_offset):
+	def calc_new_speed(self, rest_distance, rest_time, time_offset):
 
 		#add the offset to the rest time 
 		rest_time = rest_time + time_offset
@@ -220,15 +255,97 @@ class Drive_Algorithm:
 		new_speed = rest_distance / rest_time
 		return new_speed
 
+	# Start of the Main Loop in this Thread
+	def run(self):
+		
+		while (1):
+			global mqtt_return_value        # declare the global var for the class
+			global workArray				# saves the actual times for the Trafficlight phases
+			global serial					# Serial connection to the Mbot
+			global m_start_signal			# Variable to save the start signal for the Mbot 
+		
+			#Now prepare the Variables we needed for the Calculation
+		
 
-	"""
-		Now prepare the Variables we needed for the Calculation
-	"""
-	#time to next greed start give us the time till the next green phase beginns. 
-	#This Var is a Timer with an intervall of 10+1+3+10+2 = 26Sek.
-	time_to_next_green_start = 26.0 
-	print self.timeNextGreenStart
-	
+			#there are a few given variables 
+
+			#time to next greed start give us the time till the next green phase beginns. 
+			#This Var is a Timer with an intervall of 10+1+3+10 = 24Sek.
+		
+			time_to_next_green_start = self.timeNextGreenStart()
+			
+			#print time_to_next_green_start
+			
+			time_to_next_green_start = float(time_to_next_green_start)
+			
+			#time intervall for the traffic light loop
+			time_green_interval = 24
+
+			#Max Power Speed is the Factor in M/s with MbotDrive(255 forward)
+			max_power_speed = 0.42
+
+			#seed is the setted maximum speed 
+			speed = 255
+
+			#time t is where the Mbot makes a Dessicion 
+			time_decide = 5
+
+			#distance from the Startingpoint to the Trafficlight
+			distance_start_trafficlight = 5
+
+			#time Offset to reach the trafficlight a little bit after switching
+			time_offset = 0
+
+			#Calc the first reachable green phase
+			earliest_green_phase = self.calc_reachable_green_phase(max_power_speed, distance_start_trafficlight, time_to_next_green_start, time_green_interval)
+			
+			#than calc the rest distance after time decision 
+			rest_distance = self.calc_rest_distance(max_power_speed, distance_start_trafficlight, time_decide)
+			
+			#calc the time from t_desicion to time earliest green phase 
+			rest_time = earliest_green_phase - time_decide
+			
+			#now calc the new Speed after time decision 
+			new_speed = self.calc_new_speed(rest_distance, rest_time, time_offset)
+			
+			#now transfer the M/S value in an Mbot Vlaue
+			new_mbot_speed = self.speed2int(new_speed, max_power_speed)
+			
+			
+			
+			print "Der Mbot Faehrt los"
+			print int(speed)
+			# Start the Mbot with the max. Speed
+			mbot_drive_straight(serial,int(speed) ,"forward")
+			
+			print "Wartet time_decide Sekunden"
+			print time_decide
+			
+			#Wait for time_decide Seconds 
+			time.sleep(time_decide)
+			"""
+			print "Geschwindigkeitsanpassung auf:"
+			print int(new_mbot_speed)
+			
+			for x in range (speed,new_mbot_speed,-1):
+				mbot_drive_straight(serial,int(x) ,"forward")
+				time.sleep(0.05)
+			"""
+			
+			#Set new calculted Speed to the Mbot 
+			mbot_drive_straight(serial,int(new_mbot_speed) ,"forward")
+			
+			print "wartet wieder "
+			print rest_time
+			#Wait for rest_time seconds 
+			time.sleep(rest_time)
+			
+			print "Mbot stoppt"
+			#After arriving at the traffic light, Stop the Motor
+			mbot_motor_stop(serial )
+			
+			
+			time.sleep(10)
 
 
 
@@ -260,10 +377,7 @@ class Drive_Algorithm:
 			return newMbotSpeed  
 
 	def run(self):                      # beginning of the "Main Loop" From these Thread
-		global mqtt_return_value        # declare the global var for the class
-		global workArray
-		global serial
-		global m_start_signal
+
 		
 		
 		while self._running:            # Start While Loop 
@@ -357,28 +471,7 @@ class Drive_Algorithm:
 							mbot_motor_stop(serial)
 							break
 					
-	def timeNextGreenStart(self):
-		#Function returns a float with time in seconds till the next Green-Phase ends
-		if 	workArray[5] == "Green":
-			return 0 
-			print("--------------------------------------------------Green received in main.py")
 
-		if workArray[5] == "Red":
-			#return (ttns + timeRedYellow)
-			return (workArray[4] + workArray[1])
-			print("--------------------------------------------------Red received in main.py")
-
-		if workArray[5] == "Yellow":
-			#return (ttns + timeRed + timeRedYellow)
-			return (workArray[4] + workArray[0] + workArray[1])
-			print("--------------------------------------------------Yellow received in main.py")
-			
-		if workArray[5] == "Red-Yellow":
-			#return ttns
-			return workArray[4]
-			print("--------------------------------------------------Red-Yellow received in main.py")
-		else:
-			return "Time till next Green phase can not be calculated"
 		
 	def slowdown(self):
 		for new_speed in range(self.start_speed,self.min_speed):
@@ -401,7 +494,7 @@ Get_MQTT_Thread = Thread(target=Get_MQTT.run)
 #Start Thread Get_MQTT_Thread
 Get_MQTT_Thread.start()
 
-sleep (1)							#Wait till next 
+
 #Create Class Drive_Algortihm
 Drive_Algorithm = Drive_Algorithm()
 #Create Thread Drive_Algortihm_Thread
@@ -415,24 +508,25 @@ Drive_Algorithm_Thread.start()          #The Drive Algorithm should be starded l
 """
 
 
-[10.0, 1.0, 3.0, 10.0, 2.0, '']
+#[10.0, 1.0, 3.0, 10.0, 2.0, '']
 
-Exit = False                            # Exit flag for Breaking up with the main Loop 
-while Exit==False:                      # while Exit flag == False => Do the Loop... A Possible Exit Situation is when the Mbot reaches its goal
+
+#Exit = False                            # Exit flag for Breaking up with the main Loop 
+#while Exit==False:                      # while Exit flag == False => Do the Loop... A Possible Exit Situation is when the Mbot reaches its goal
     #Starting the Main Loop of the whole Programm 
 
-    """
-	This is the Main Loop...
-	This is where the magic happened
-    """
+#	"""
+#	This is the Main Loop...
+#	This is where the magic happened
+#	"""
 	
     #The Get_MQTT Thread is already starded if there is there is the Start Topic, Start the Drive Algorithm
-    if (m_start_signal == 1):
+if (m_start_signal == 1):
 	#Start Thread Drive_Algortihm_Thread
-		t0 = timeit.default_timer()
-		Drive_Algorithm_Thread.start() 
+		#t0 = timeit.default_timer()
+	Drive_Algorithm_Thread.start() 
 
-
+while(1):
     """
     ############### Some Test Code 
     print "Hier meldet sich die Main zu wort"
